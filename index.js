e930cf7a973cab733209bcade51e7b81a53961a7
JI-RIDOY <aridoy102536@gmail.com>
2025-12-30 19:28:33 +0600

    deploy done

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const port = process.env.PORT || 5000;
 // Socket.io configuration
 const io = new Server(server, {
   cors: {
-    origin: " http://localhost:5173",
+    origin: " https://creative-career-ai.web.app",
     credentials: true
   }
 });
 
 // Middleware
 app.use(cors({
-  origin: " http://localhost:5173",
+  origin: " https://creative-career-ai.web.app",
   credentials: true
 }));
 app.use(express.json());
